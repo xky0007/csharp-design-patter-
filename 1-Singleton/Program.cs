25852bf864a5421d8ebd25f2f8d41d68313dedd7
xky0007 <xky0007@gmail.com>
2018-10-17 13:16:23 -0400

    1st

---
+++ 1-Singleton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Singleton
+    {
+        private static Singleton _instance;
+
+        //can't be init with construction
+        private Singleton(){}
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if(_instance ==null)
+                    _instance = new Singleton();
+                return _instance; // always return the same instance
+            }
+        }
+    }
+}
